fcbeba5640f01acdba07cbc1d937f633b73ea496
seeys <es2008e@naver.com>
2021-11-11 23:46:53 +0900

    [update] insert, delete 버튼 추가

--- src/containers/Table/Approval.jsx
+++ src/containers/Table/Approval.jsx
@@ -1,23 +1,87 @@
 import React, { Component } from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import oc from 'open-color';
 // 결재 목록 보여주는 페이지 -> 결재 승인하는 상세페이지로 들어감.
 class Approval extends Component {
     
-    handleInsertButtonClick = (onClick) => {
+    handleInsertButtonClick = async () => {
         console.log('insert clcik');
-        onClick();
+        fetch("http://localhost:3000/approval/insert", {
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json",
+            },
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((e) => {
+            console.log(e);
+        }
+        )
     }
+
     createCustomInsertButton = (onClick) => {
         return (
-            <button style={ { color: 'red' } } onClick={ onClick }>Add rows</button>
+            <button style={ { background: oc.blue[4] } } onClick={ onClick }>INSERT</button>
         );
     }
-      
+    
+    handleDeleteButtonClick = async () => {
+        console.log('insert clcik');
+        fetch("http://localhost:3000/approval/delete", {
+            method:"DELETE",
+            headers:{
+                "Content-Type":"application/json",
+            },
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((e) => {
+            console.log(e);
+        }
+        )
+    }
+    
+    createCustomDeleteButton = (onClick) => {
+        return (
+            <button style={ { color: oc.blue[4] } } onClick={ onClick }>DELETE</button>
+        );
+    }
+
+    handleUpdateButtonClick = async () => {
+        console.log('insert clcik');
+        fetch("http://localhost:3000/approval/update", {
+            method:"UPDATE",
+            headers:{
+                "Content-Type":"application/json",
+            },
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((e) => {
+            console.log(e);
+        }
+        )
+    }
+    
+    createCustomUpdateButton = (onClick) => {
+        return (
+            <button style={ { color: oc.blue[4] } } onClick={ onClick }>Update</button>
+        );
+    }
+
     render (){
         const options = {
             exportCSVText: 'export',
             insertBtn: this.createCustomInsertButton,
-            deleteText: 'delete'
+            deleteBtn: this.createCustomDeleteButton,
+            updateBtn: this.createCustomUpdateButton
         };
     
         const selectRowProp = {
@@ -55,7 +119,7 @@ class Approval extends Component {
         }];
         return (
             <BootstrapTable data={ products } search={true} multiColumnSearch={true}
-            options={options} selectRow={selectRowProp } insertRow deleteRow exportCSV >
+            options={options} selectRow={ selectRowProp } insertRow deleteRow exportCSV >
                 <TableHeaderColumn width='100' dataField='id' isKey>ID</TableHeaderColumn>
                 <TableHeaderColumn width='100'dataField='shortname'>약자</TableHeaderColumn>
                 <TableHeaderColumn width='200' dataField='engname'>영문명</TableHeaderColumn>
