dd797f84d0ce042b8201316b1849e827125dc705
seeys <es2008e@naver.com>
2021-11-13 00:15:47 +0900

    [update] insert 수정

--- src/containers/Table/Approval.jsx
+++ src/containers/Table/Approval.jsx
@@ -1,37 +1,34 @@
 import React, { Component } from 'react';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import {BootstrapTable, TableHeaderColumn, InsertModalFooter, InsertButton} from 'react-bootstrap-table';
 import oc from 'open-color';
 // 결재 목록 보여주는 페이지 -> 결재 승인하는 상세페이지로 들어감.
 class Approval extends Component {
-    
-    handleInsertButtonClick = async () => {
-        console.log('insert clcik');
-        fetch("http://localhost:3000/approval/insert", {
-            method:"GET",
-            headers:{
-                "Content-Type":"application/json",
-            },
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((e) => {
-            console.log(e);
-        }
-        )
-    }
 
+    handleInsertButtonClick = (onClick) => {
+        // Custom your onClick event here,
+        // it's not necessary to implement this function if you have no any process before onClick
+        console.log('This is my custom function for InserButton click event');
+        onClick();
+      }
+    
     createCustomInsertButton = (onClick) => {
         return (
-            <button style={ { background: oc.blue[4] } } onClick={ onClick }>INSERT</button>
+          <InsertButton
+            btnText='CustomInsertText'
+            btnContextual='btn-warning'
+            className='my-custom-class'
+            btnGlyphicon='glyphicon-edit'
+            onClick={ () => this.handleInsertButtonClick(onClick) }
+            >Insert</InsertButton>
         );
     }
     
-    handleDeleteButtonClick = async () => {
-        console.log('insert clcik');
-        fetch("http://localhost:3000/approval/delete", {
-            method:"DELETE",
+    handleSave(save) {
+        // Custom your onSave event here,
+        // it's not necessary to implement this function if you have no any process before save
+        console.log('save clcik');
+        fetch("http://localhost:3000/approval/insert", {
+            method:"POST",
             headers:{
                 "Content-Type":"application/json",
             },
@@ -44,35 +41,20 @@ class Approval extends Component {
             console.log(e);
         }
         )
+        save();
     }
     
-    createCustomDeleteButton = (onClick) => {
+    createCustomModalFooter = (closeModal, save) => {
         return (
-            <button style={ { color: oc.blue[4] } } onClick={ onClick }>DELETE</button>
-        );
-    }
-
-    handleUpdateButtonClick = async () => {
-        console.log('insert clcik');
-        fetch("http://localhost:3000/approval/update", {
-            method:"UPDATE",
-            headers:{
-                "Content-Type":"application/json",
-            },
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((e) => {
-            console.log(e);
-        }
-        )
-    }
-    
-    createCustomUpdateButton = (onClick) => {
-        return (
-            <button style={ { color: oc.blue[4] } } onClick={ onClick }>Update</button>
+          <InsertModalFooter
+            className='my-custom-class'
+            saveBtnText='CustomSaveText'
+            closeBtnText='CustomCloseText'
+            closeBtnContextual='btn-warning'
+            saveBtnContextual='btn-success'
+            closeBtnClass='my-close-btn-class'
+            saveBtnClass='my-save-btn-class'
+            onSave={ () => this.handleSave(save) }/>
         );
     }
 
@@ -80,8 +62,7 @@ class Approval extends Component {
         const options = {
             exportCSVText: 'export',
             insertBtn: this.createCustomInsertButton,
-            deleteBtn: this.createCustomDeleteButton,
-            updateBtn: this.createCustomUpdateButton
+            insertModalFooter: this.createCustomModalFooter
         };
     
         const selectRowProp = {
@@ -93,38 +74,34 @@ class Approval extends Component {
             shortname: "APT",
             engname: "Advanced Persistent Threat",
             korname: "지능형 타깃 지속 공격",
-            index: "1541",
             meaning: "ㄱㄴㄷㄻㅊ"
         }, {
             id: 2,
             shortname: "APT",
             engname: "Advanced Persistent Threat",
             korname: "지능형 타깃 지속 공격",
-            index: "1541",
             meaning: "ㄱㄴㄷㄻㅊ"
         }, {
             id: 3,
             shortname: "APT",
             engname: "Advanced Persistent Threat",
             korname: "지능형 타깃 지속 공격",
-            index: "1541",
             meaning: "ㄱㄴㄷㄻㅊ"
         }, {
             id: 4,
             shortname: "APT",
             engname: "Advanced Persistent Threat",
             korname: "지능형 타깃 지속 공격",
-            index: "1541",
             meaning: "ㄱㄴㄷㄻㅊ"
         }];
+
         return (
             <BootstrapTable data={ products } search={true} multiColumnSearch={true}
-            options={options} selectRow={ selectRowProp } insertRow deleteRow exportCSV >
+            options={options} selectRow={ selectRowProp } insertRow  exportCSV >
                 <TableHeaderColumn width='100' dataField='id' isKey>ID</TableHeaderColumn>
                 <TableHeaderColumn width='100'dataField='shortname'>약자</TableHeaderColumn>
                 <TableHeaderColumn width='200' dataField='engname'>영문명</TableHeaderColumn>
                 <TableHeaderColumn width='200' dataField='korname'>한글명</TableHeaderColumn>
-                <TableHeaderColumn width='100' dataField='index'>구분자</TableHeaderColumn>
                 <TableHeaderColumn width='300' dataField='meaning'>설명</TableHeaderColumn>
             </BootstrapTable>
     
