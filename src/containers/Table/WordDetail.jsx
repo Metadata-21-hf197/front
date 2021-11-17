3aadaa49171c10167e41e07a836505b4e94aba7f
uoise <yw823x@daum.net>
2021-11-17 19:36:04 +0900

    [update] Update, Detail Page

--- src/containers/Table/WordDetail.jsx
+++ src/containers/Table/WordDetail.jsx
@@ -1,7 +1,7 @@
 // 결재 상세 페이지
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { ApprovalWithLabel, ApprovalInputLabel } from '../../components/Table';
+import { ApprovalWithLabel } from '../../components/Table';
 import ApprovalButton from '../../components/Table/ApprovalButton';
 import ApprovalContent from '../../components/Table/ApprovalContent';
 import axios from 'axios';
@@ -9,85 +9,64 @@ import axios from 'axios';
 let u_id;
 
 class WordDetail extends Component {
-
     state = {
-            korName:'',
-            engName:'',
-            shortName:'',
-            meaning:''
+        korName: "",
+        engName: "",
+        shortName: "",
+        meaning: "",
+        createUser: "",
+        createDate: "",
+        modifyUser: "",
+        modifyDate: ""
     };
 
-    loadData = async () => {
-        const { location } = this.props;
-        u_id = location.props.id;
-        
+    loadData = async(props) => {
+        u_id = this.props.match.params.id;
         axios
           .get(`/table/word/${u_id}`)
-          .then(({ data }) => {
-            this.setState({ 
-              korName: data.word.korName,
-              engName: data.word.engName,
-              shortName: data.word.shortName,
-              meaning: data.word.meaning,
-            });
+          .then(({ data }) => {  
+                this.setState({
+                korName: data.word.korName,
+                engName: data.word.engName,
+                shortName: data.word.shortName,
+                meaning: data.word.meaning,
+                createUser: data.word.createUser.memberName,
+                createDate: data.word.createDate
+                });
+
+                if(data.word.modifyUser != null){
+                    this.setState({
+                    modifyUser: data.word.modifyUser.memberName,
+                    modifyDate: data.word.modifyDate
+                    });
+                } else {
+                    this.setState({
+                        modifyUser: " ",
+                        modifyDate: " "
+                });
+            }     
           })
           .catch(e => {  // API 호출이 실패한 경우
             console.error(e);  // 에러표시
           });
-      };
+    };
 
     componentWillMount() {
         this.loadData();
     }
-    
-    handleChange = (e) => {
-        this.setState({
-                [e.target.name] : e.target.value
-        });
-        console.log(this.state);
-    }
-    
-    confirmClick = () => {
-        //결재 승인 버튼
-        // /word/{wordId} put
-        
-        fetch(`/word/${u_id}`, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              "shortName": this.state.shortName,
-              "engName": this.state.engName,
-              "korName": this.state.korName,
-              "meaning": this.state.meaning
-            }),
-          })
-            .then((data) => {
-              console.log(data);
-            })
-            .catch((e) => console.log(e));
-    }
 
     render (){
-        const { location } = this.props;
         return (
             <Box1>
                     <ApprovalContent title="기존">
-                        <ApprovalWithLabel label="id" val={location.props.id}></ApprovalWithLabel>
-                        <ApprovalWithLabel label="kor" val={location.props.k} ></ApprovalWithLabel>
-                        <ApprovalWithLabel label="eng" val={location.props.e}></ApprovalWithLabel>
-                        <ApprovalWithLabel label="short" val={location.props.s}></ApprovalWithLabel>
-                        <ApprovalWithLabel label="meaning" val={location.props.m}></ApprovalWithLabel>
-                    </ApprovalContent>
-                    <ApprovalContent title="수정">
-                        <ApprovalInputLabel label="kor" value={this.state.korName} onChange={this.handleChange} name="korName" type="text"/>
-                        <ApprovalInputLabel label="eng" value={this.state.engName} onChange={this.handleChange} name="engName" type="text"/>
-                        <ApprovalInputLabel label="short" value={this.state.shortName}  onChange={this.handleChange} name="shortName" type="text"/>
-                        <ApprovalInputLabel label="meaning" value={this.state.meaning} onChange={this.handleChange} name="meaning" type="text"/>
-                    </ApprovalContent>
-                    <ApprovalContent>
-                        <ApprovalButton onClick={this.confirmClick}>수정신청</ApprovalButton>
+                        <ApprovalWithLabel label="kor" value={this.state.korName}></ApprovalWithLabel>
+                        <ApprovalWithLabel label="eng" val={this.state.engName}></ApprovalWithLabel>
+                        <ApprovalWithLabel label="short" val={this.state.shortName}></ApprovalWithLabel>
+                        <ApprovalWithLabel label="meaning" val={this.state.meaning}></ApprovalWithLabel>
+                        <ApprovalWithLabel label="작성일" val={this.state.createDate}></ApprovalWithLabel>
+                        <ApprovalWithLabel label="수정일" val={this.state.modifyDate}></ApprovalWithLabel>
+                        <ApprovalWithLabel label="작성자" val={this.state.createUser}></ApprovalWithLabel>
+                        <ApprovalWithLabel label="수정자" val={this.state.modifyUser}></ApprovalWithLabel>
                     </ApprovalContent>
                 </Box1>
         )
